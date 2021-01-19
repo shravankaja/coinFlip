08fdb7129ed109135719a771f9c213cbc83f2ab7
shravan-kaja <shravan.kaja@gmail.com>
2021-01-19 20:45:51 +0530

    Added: Head or Tail Win limit

--- coinFlip/coinFlipSimulator.sh
+++ coinFlip/coinFlipSimulator.sh
@@ -6,10 +6,12 @@ tWin=0
 if [ $rand -eq 0 ]
 then
 	echo "Heads Wins"
-	hwin=$(($hwim + 1))
+	hwin=$(($hwin + 1))
+	echo $hwin
 else
 	echo "Tails Wins"
 	twin=$(($twin+1))
+	echo $twin
 fi
 }
 function coinFlipSimulation() {
@@ -17,7 +19,12 @@ echo "Enter number of times to execute"
 read n
 for (( i=0 ; $i < n ; i++ ))
 do
-	coinFlip
+	if [ $hwin -eq 21  -o  $twin -eq 21 ] 
+	then
+		break
+	else
+	     coinFlip
+	fi
 done
 }
 coinFlip
